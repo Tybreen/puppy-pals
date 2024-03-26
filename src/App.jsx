46c5d1b70691c0188ad53397188e9390a48ab898
Tybreen <tyler.breen@icloud.com>
2024-03-25 23:41:51 -0400

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
 import './App.css'
+import { useState } from 'react'
+import { puppyList } from './data'
 
 function App() {
   
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null)
+
+  console.log(puppies);
+
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+  console.log(`featuredPup:`, featuredPup);
 
   return (
     <>
-
+      { 
+        puppies.map((puppy) => {
+          return <p onClick={()=>{setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>
+        })
+      }
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
     </>
   )
 }
